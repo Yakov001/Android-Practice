926e0b45be5fe4c222dd1c87abb91ee8b5e997db
Yakov <yakovrussian@gmail.com>
2020-10-03 18:04:57 +0300

    Adding Stopwatch app

---
+++ Stopwatch/app/src/main/java/com/bignerdranch/android/stopwatch/MainActivity.java
@@ -0,0 +1,43 @@
+package com.bignerdranch.android.stopwatch;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.os.SystemClock;
+import android.view.View;
+import android.widget.Chronometer;
+
+public class MainActivity extends AppCompatActivity {
+    private Chronometer chronometer;
+    private boolean running;
+    private long pauseOffset;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        chronometer= findViewById(R.id.chronometer);
+    }
+
+    public void startChronometer(View view) {
+        if (!running){
+            chronometer.setBase(SystemClock.elapsedRealtime() - pauseOffset);
+            chronometer.start();
+            running = true;
+        }
+    }
+
+    public void stopChronometer(View view) {
+        if (running){
+            chronometer.stop();
+            pauseOffset = SystemClock.elapsedRealtime() - chronometer.getBase();
+            running = false;
+        }
+    }
+
+    public void resetChronometer(View view) {
+        chronometer.setBase(SystemClock.elapsedRealtime());
+        pauseOffset = 0;
+    }
+}
